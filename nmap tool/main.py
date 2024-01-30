738945fc4e1c289eea61e9965549a1386f504f21
Krimoo20 <98287762+Krimoo20@users.noreply.github.com>
2024-01-30 17:07:03 +0100

    ipAdressValidation-portRangeValidation-scanTypeValidation

--- nmap tool/main.py
+++ nmap tool/main.py
@@ -1,34 +1,57 @@
 import nmap
-scanner = nmap.PortScanner()
+import ipaddress
+import re
 
 print("Welcome this is a simple automation tool for penetration tests")
 print("<================================================================>")
-ip_adresse = input("Enter the IP address to be scanned: ")
+port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
 
-scan = input("""\n Please enter the type of scan you want to run 
-                   1)SYN ACK Scan 
-                   2)UDP Scan 
-                   3) Comprehensive Scan\n""")
+while True:
+
+    ip_address = input("Enter the IP address to be scanned: ")
+    port_range = input("Enter the ports range you want to scan !!")
+
+    try:
+
+        ip_address_is_valid = ipaddress.IPv4Address(ip_address)
+        ports_range_is_valid = port_range_pattern.match(port_range.replace(" ", ""))
+        if ip_address_is_valid and ports_range_is_valid:
+            print("You entered a valid ip address and a valid ports range !!")
+            break
+
+    except:
+
+        print("You entered an invalid ip address !!")
+
+scanner = nmap.PortScanner()
+while True:
+    scan = input("""\n Please enter the type of scan you want to run 
+                       1)SYN ACK Scan 
+                       2)UDP Scan 
+                       3) Comprehensive Scan\n""")
+    if scan <= "3":
+        break
+
 if (scan == "1"):
-    print("Nmap version: ",scanner.nmap_version())
-    scanner.scan(ip_adresse,"1-1024","-v -sS")
+    print("Nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_address, port_range, "-v -sS")
     print(scanner.scaninfo())
-    print("IP status:",scanner[ip_adresse].state())
-    print(scanner[ip_adresse].all_protocols())
-    print("Open Ports", scanner[ip_adresse]["tcp"].keys())
+    print("IP status:", scanner[ip_address].state())
+    print(scanner[ip_address].all_protocols())
+    print("Open Ports", scanner[ip_address]["tcp"].keys())
 elif (scan == "2"):
     print("Nmap version: ", scanner.nmap_version())
-    scanner.scan(ip_adresse, "1-1024", "-v -sU")
+    scanner.scan(ip_address, port_range, "-v -sU")
     print(scanner.scaninfo())
-    print("IP status:", scanner[ip_adresse].state())
-    print(scanner[ip_adresse].all_protocols())
-    print("Open Ports", scanner[ip_adresse]["udp"].keys())
+    print("IP status:", scanner[ip_address].state())
+    print(scanner[ip_address].all_protocols())
+    print("Open Ports", scanner[ip_address]["udp"].keys())
 elif (scan == "3"):
     print("Nmap version: ", scanner.nmap_version())
-    scanner.scan(ip_adresse, "1-1024", "-v -sS -sV -sC -A -O")
+    scanner.scan(ip_address, port_range, "-v -sS -sV -sC -A -O")
     print(scanner.scaninfo())
-    print("IP status:", scanner[ip_adresse].state())
-    print(scanner[ip_adresse].all_protocols())
-    print("Open Ports", scanner[ip_adresse]["udp"].keys())
+    print("IP status:", scanner[ip_address].state())
+    print(scanner[ip_address].all_protocols())
+    print("Open Ports", scanner[ip_address]["udp"].keys())
 else:
     print("Please enter an appropriate option !!")
